c1998f422278378c643e919d7d6bd093e3f9572b
WeizhuoZhang <wzhuo.zhang@gmail.com>
2019-11-20 21:09:01 +1100

    [ADD] add chapter 7 notebook code 7.10-7.13

---
+++ chapter07_function_basics/notebook_codes/code_7_13_strctptr.cpp
@@ -0,0 +1,63 @@
+// strctfun.cpp -- functions with a structrure argument
+#include <iostream>
+#include <cmath>
+
+struct polar
+{
+    double distance;
+    double angle;
+};
+
+struct rect
+{
+    double x;
+    double y;
+};
+
+void rect_to_polar(const rect * pxy, polar * pda);
+void show_polar(const polar * pda);
+
+int main()
+{
+    using namespace std;
+    rect rplace;
+    polar pplace;
+
+    cout << "Enter the x and y values: ";
+    while (cin >> rplace.x >> rplace.y)
+    {
+        rect_to_polar(&rplace, &pplace);
+        show_polar(&pplace);
+        cout << "Next two numbers (q to quit): ";
+    }
+    cout << "Done. \n";
+    return 0;
+}
+
+void rect_to_polar(const rect * pxy, polar * pda)
+{
+    using namespace std;
+    pda->distance =
+        sqrt(pxy->x * pxy->x + pxy->y * pxy->y);
+    pda->angle = atan2(pxy->y, pxy->x);
+}
+
+void show_polar(const polar * pda)
+{
+    using namespace std;
+    const double Rad_to_deg = 57.29577951;
+
+    cout << "distance = " << pda->distance;
+    cout << ", angle = " << pda->angle * Rad_to_deg;
+    cout << " degrees.\n";
+}
+
+/* Output
+ *
+ * Enter the x and y values: 30 40
+ * distance = 50, angle = 53.1301 degrees.
+ * Next two numbers (q to quit): -100 100
+ * distance = 141.421, angle = 135 degrees.
+ * Next two numbers (q to quit): q
+ * Done.
+ */
